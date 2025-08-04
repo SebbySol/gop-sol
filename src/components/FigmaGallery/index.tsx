5e10697c9c3b6a5c48839981dc07cec71adbdb3f
RjTabiano <tabianorj@gmail.com>
2025-08-04 16:48:20 +0800

    Spacing improvements

--- src/components/FigmaGallery/index.tsx
+++ src/components/FigmaGallery/index.tsx
@@ -6,13 +6,13 @@ import "swiper/css";
 
 export default function FigmaGallery() {
     return (
-        <section className="bg-[#FF5500] pb-35">
+        <section className="bg-[#FF5500] lg:pb-35">
 
             <div className="bg-black lg:rounded-bl-[600px] lg:pb-50 pb-20">
                 <h2 className="text-white text-6xl md:text-7xl font-bold text-center lg:text-right lg:mr-10 mb-15 pt-20 pb-5">
                     Figma
                 </h2>
-                <div className="w-full lg:ml-65">
+                <div className="w-full lg:ml-65 px-4">
                     <Swiper
                         modules={[Autoplay]}
                         slidesPerView={3}
@@ -42,7 +42,7 @@ export default function FigmaGallery() {
                             },
                             1024: {
                                 slidesPerView: 3,
-                                spaceBetween: 45,
+                                spaceBetween: 25,
                             },
                         }}
                     >
@@ -55,7 +55,7 @@ export default function FigmaGallery() {
                                     <img
                                         src={project.src}
                                         alt={project.alt}
-                                        className="w-[80%] sm:w-[60%] md:w-full h-[250px] sm:h-[400px] md:h-[450px] object-cover rounded-3xl transition-transform duration-300 hover:scale-105 shadow-lg"
+                                        className="w-[80%] sm:w-[100%] md:w-[100%] h-[250px] sm:h-[320px] md:[300px] lg:h-[280px] xl:h-[320px] 2xl:h-[450px] object-fill rounded-3xl transition-transform duration-300 hover:scale-105 shadow-lg"
                                     />
                                     <div className="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-all duration-300" />
                                     <div className="absolute bottom-0 left-0 right-0 p-3 bg-gradient-to-t from-black/80 to-transparent rounded-b-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300">
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,10 @@ export default function Footer() {
     return (
         <footer className="bg-black text-white py-15 px-6">
             <div className="container mx-auto text-center">
-                <p className="text-2xl">
+                <p className="text-base md:text-lg lg:text-2xl">
                     &copy; {new Date().getFullYear()} SebbySol. All rights reserved.
                 </p>
-                <p className="text-xs mt-2">
+                <p className="text-xs md:text-base mt-2">
                     Maangas at Matikas na pinoy developer.
                 </p>
             </div>
